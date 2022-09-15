276bef069e882f08885e5895eff22521179033bd
Yoni Silvestre <yonisantiagosg@gmail.com>
2022-09-15 18:41:01 -0400

    [Add]Files added

---
+++ src/components/Header.jsx
@@ -0,0 +1,92 @@
+import React, {useState, useContext} from 'react';
+import { Menu } from './Menu';
+import '@styles/Header.scss';
+import Logo from '@logos/logo_yard_sale.svg'
+import menuIcon from '@icons/icon_menu.svg';
+import Cart from '@icons/icon_shopping_cart.svg'
+import { Link, NavLink } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+import { MyOrder } from '../containers/MyOrder';
+import { MobileMenu } from './MobileMenu';
+
+export const Header = () => {
+    
+
+    const [toggle, setToggle] = useState(false);//show user menu my orders, my account, sign out
+
+    const [toggleOrders, setToggleOrders] = useState(false);// show products in cart orders
+
+    const [toggleNavBar, setToggleNavBar] = useState(false)// show menu in hamburger toggle 
+
+    const {state} = useContext(AppContext);
+
+    const handleToggle = () =>{
+        setToggle(!toggle);
+
+    };
+
+    const toggleMobileMenu = () =>{
+        toggleNavBar && setToggleNavBar(!toggleNavBar)
+    }
+
+    // const handleToggleNavBar = () => {
+    //     setToggleNavBar(!toggleNavBar)
+    // }
+
+
+    return (
+
+      <nav>
+    
+            <img src={menuIcon} alt="menu" className="menu" onClick={()=>setToggleNavBar(!toggleNavBar)}/>
+      
+
+         
+          <div className="navbar-left">
+
+                <Link to="/">
+                    <img src={Logo} alt="logo" className="nav-logo" />
+                </Link>
+
+              
+              <ul>
+                  <li>
+                      <a href="/">All</a>
+                  </li>
+                  <li>
+                      <a href="/">Clothes</a>
+                  </li>
+                  <li>
+                      <a href="/">Electronics</a>
+                  </li>
+                  <li>
+                      <a href="/">Furnitures</a>
+                  </li>
+                  <li>
+                      <a href="/">Toys</a>
+                  </li>
+                  <li>
+                      <a href="/">Others</a>
+                  </li>
+              </ul>
+          </div>
+          <div className="navbar-right">
+              <ul>
+                  <li className="navbar-email" onClick={handleToggle}>
+                     user@example.com
+                  </li>
+                  <li className="navbar-shopping-cart" onClick={() => setToggleOrders(!toggleOrders)}>
+                      <img src={Cart} alt="shopping cart" />
+                     {state.cart.length > 0 ? <div>{state.cart.length}</div> : null }
+                  </li>
+              </ul>
+          </div>
+          {toggleNavBar && <MobileMenu toggleMobileMenu={toggleMobileMenu}/>}
+            { toggle && <Menu />}
+            { toggleOrders && <MyOrder toggleOrders={toggleOrders} setToggleOrders={setToggleOrders}/>}
+            
+          
+      </nav>
+
+  )
+}
---
+++ src/routes/App.jsx
@@ -0,0 +1,47 @@
+
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Home } from '../pages/Home';
+import { NotFound } from '../pages/NotFound';
+import { Layout } from '../containers/Layout';
+import { Login } from '../pages/Login';
+import { RecoveryPassword } from '../pages/RecoveryPassword';
+import {CreateAccount} from '../pages/CreateAccount';
+import { SendEmail } from '../pages/SendEmail';
+import '../styles/global.css'
+import { MyAccount } from '../pages/MyAccount';
+import { Orders } from '../pages/Orders';
+import { Checkout } from '../pages/Checkout';
+import { NewPassword } from '../pages/NewPassword';
+import {AppContext} from '../context/AppContext.js';
+import {useInitialState} from '../hooks/useInitialState.js';
+
+export const App = () => {
+  const initialState = useInitialState()
+  return (
+  
+    <AppContext.Provider value={initialState}>
+    <BrowserRouter>
+    <Layout>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/password-recovery' element={<RecoveryPassword/>}/>
+          <Route path='/new-password' element={<NewPassword/>} />
+          <Route path='/send-email' element={<SendEmail/>}/>
+          <Route path='/signup' element={<CreateAccount/>}/>
+          <Route path='/account' element={<MyAccount/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+          <Route path='/checkout' element={<Checkout/>} />
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+    </AppContext.Provider>
+      
+  )
+};
